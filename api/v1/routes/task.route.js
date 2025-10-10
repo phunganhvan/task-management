fd4194c119238df5cc170820ed5dae187168b9a7
Phùng Anh Văn <phunganhvan13122004@gmail.com>
2025-10-10 17:02:05 +0700

    chinh sua cong viec api

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -10,4 +10,6 @@ router.patch('/change-status/:id', controller.changeStatus)
 router.patch('/change-multi', controller.changeMulti);
 
 router.post('/create', controller.create);
+
+router.patch("/edit/:id", controller.edit)
 module.exports = router;
