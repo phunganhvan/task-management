08b247277aeba7efeeb384a0d745ce5d7da06fc3
Phùng Anh Văn <phunganhvan13122004@gmail.com>
2025-10-13 13:37:50 +0700

    cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express= require('express');
 require("dotenv").config();
 const bodyParser= require('body-parser')
+const cors = require('cors');
 const app= express()
 
-
+app.use(cors());
 
 const port= process.env.PORT;
 const route= require("./api/v1/routes/index.route");
